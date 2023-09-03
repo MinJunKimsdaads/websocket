9b11f7e9ba5332f0b650b7ad66ca54d96bb28e72
MinJunKimsdaads <aaeexx@naver.com>
2023-09-04 08:28:20 +0900

    20230904

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import { useState } from 'react';
+import { io } from 'socket.io-client';
+const socket = io("http://localhost:3001",{
+  rejectUnauthorized: false
+}); //백엔드 서버 포트를3001와 socket연결 
+
 
 function App() {
+  const initialValue = {
+    name: "",
+    msg:"",
+  }
+  
+  const [val, setValue] = useState<any>(initialValue);
+
+  const {name, msg} = val;	//비구조화 할당
+  
+  const [msgList, setMsgList] = useState<any[]>([]);
+  
+ 
+
+  
+  const sendMessage = (e:any) => {
+    e.preventDefault();
+    socket.emit("send message",{
+      name: val.name,
+      msg: val.msg,
+    })
+  
+    setValue(initialValue);
+  }
+  
+  const messageComponentMount = () => {
+    socket.on("receive message",(message)=>{
+      setMsgList([...msgList,message]);
+    })
+  }
+  
+  const onChange = (e:any) => {
+    const {value, name:name} = e.target;
+    setValue({...val,[name]:value});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="chat_list">
+        {msgList.map((item) => (
+          <div className="messagelist">
+            <p className="username">{item.name}</p>
+            <p className="msg_text">{item.msg}</p>
+          </div>
+        ))}
+      </section>
+      <form className="chat_con" onSubmit={sendMessage}>
+          <div className="chat_inputs">
+            <input type="text" onChange={onChange} value={val.name} name="name" placeholder="아이디"/>
+            <input type="text" onChange={onChange} value={val.msg} name="msg" placeholder="메세지내용"/>
+          </div>
+          <button className="chat_button" type="submit">
+            보내기
+          </button>
+        </form>
+      
     </div>
   );
 }
